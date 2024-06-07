2bf1b6b9818b729e7f151bc9479d750bed769035
adarshdebata <adarshdebata00@gmail.com>
2024-06-08 02:19:52 +0530

    HelloWorld Program Added

---
+++ Fundamental-Codes/HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+/*
+By writing a program that displays “Hello World,”
+ programmers can quickly verify that their development environment is correctly configured and that they have successfully grasped the basics of a new programming language.
+
+*Importance Note*
+
+Why `String[] args` in the Main Method?
+    -In Java, the `main` method is the entry point of a Java application. It has the signature `public static void main(String[] args)`. 
+    The `String[] args` parameter allows command-line arguments to be passed to the Java program. 
+    This is useful for providing inputs or options to the program when it is executed.
+
+Naming Convention
+    - Classes: Start with an uppercase letter, followed by mixed case (Pascal Naming Convention). For example, `HelloWorld`.
+    - Methods: Start with a lowercase letter, followed by mixed case (camel case naming convention). For example, `printMessage`.
+ */
