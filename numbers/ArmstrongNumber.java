19a0e47561b6efe211405b6c63afa067b9d8e61f
adarshdebata <adarshdebata00@gmail.com>
2024-06-17 15:56:22 +0530

    Armstrong Number

---
+++ numbers/ArmstrongNumber.java
@@ -0,0 +1,39 @@
+/*
+Sum of Cube of  digit = orignal/input
+*/
+import java.lang.Math;
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+
+    boolean armstrongNumber(int num){
+        int orignal = num;
+        int digit;
+        int armNum=0;
+        while (num!=0){
+            digit = num % 10;
+            //math.pow() only return double so we typecast it to int 
+            armNum += (int) Math.pow(digit, 3);  //instead of Math.pow() we can use  digit*digit*digit
+            num /= 10;
+        }
+        if (orignal == armNum)
+            return true;
+        else
+            return false;
+    }
+    
+    public static void main(String[] args) {
+        ArmstrongNumber ob = new ArmstrongNumber();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int input = sc.nextInt();
+        
+        if (ob.armstrongNumber(input))
+            System.out.println("This is an Armstrong Number");
+        else
+            System.out.println("No it's not an Armstrong Number");
+        
+            sc.close();//closing scanner object 
+    }
+
+}
