edca44691226b8111614cf54ff563b2dd24cb29f
adarshdebata <adarshdebata00@gmail.com>
2024-07-08 12:05:27 +0530

    Add ReverseNumber.java: Example for reversing a number

---
+++ numbers/ReverseNumber.java
@@ -0,0 +1,18 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int number = 12345;
+        int reversedNumber = reverse(number);
+        System.out.println("Reversed Number: " + reversedNumber);
+    }
+
+    // Method to reverse a number
+    public static int reverse(int number) {
+        int reversed = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+        return reversed;
+    }
+}
