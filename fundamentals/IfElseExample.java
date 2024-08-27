82247add78f908c80896e7ae78ca636824dc1c43
Adarsh Kumar Debata <adarsh.debata@iserveu.co.in>
2024-08-27 09:38:45 +0530

    code update

--- fundamentals/IfElseExample.java
+++ fundamentals/IfElseExample.java
@@ -1,11 +1,14 @@
-//to understand this we find +ive, -ive,zero from user input
+
+//to understand this we find +ive, -ive, zero from user input
 import java.util.Scanner;
 
 public class IfElseExample {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner;
+        scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
+
         if (number > 0) {
             System.out.println("The number is positive.");
         } else if (number < 0) {
@@ -13,6 +16,7 @@ public class IfElseExample {
         } else {
             System.out.println("The number is zero.");
         }
+
         scanner.close();
     }
 }
